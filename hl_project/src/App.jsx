6d84712e04dafc5b3355e73467713ed5b1cc7e8f
Vishakha Pare <159012315+vishakhapare@users.noreply.github.com>
2024-12-30 23:08:18 +0530

    first msg

--- hl_project/src/App.jsx
+++ hl_project/src/App.jsx
@@ -1,41 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Aboutus from './pages/Aboutus/Aboutus';
-import OurCourses from './pages/Courses/OurCourses';
+import OurCourses from './pages/Courses/Ourcourses';  
 import Contactus from './pages/Contact/Contactus';
 import Login from './pages/loginpage/Login';
 
-
-
 const App = () => {
-
   return (
-<>
-   <Router basename='/hl_project'>
-     <Routes>
-      <Route exact path='/'element={<Home/>}/>
-      <Route path='/aboutus' element={<Aboutus/>}/>
-    <Route path='/courses' element={<OurCourses/>}/>
-     <Route path='/contactus' element={<Contactus/>}/>
-     <Route path='/login'element={<Login/>}/>
-       </Routes>
-    </Router>
-     
-
-
-      
-  
-      
-    
-      
-      
-      
-      
-    
-      </>
-
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/courses" element={<OurCourses />} />
+          <Route path="/contactus" element={<Contactus />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
-export default App
+export default App;
